4f57145db42f9c04f731756479cb0933abd76372
francosalvati <francosalvati98@gmail.com>
2023-04-10 19:20:08 -0300

    entrega

--- src/api/api-cosas.js
+++ src/api/api-cosas.js
@@ -5,50 +5,50 @@ const catalogo = [
         nombre: 'Black widow remera',
         color: 'negro',
         precio: 1000,
-        stock: 200,
+        stock: 5,
         producto: 't-shirt',
         img: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/845/336/products/negra-social-atras111-955069cb5989cddd3816700060505334-640-0.jpg',
-        descrpcion: ''
+        descripcion: 'Esta es una remera negra...'
     },
     {
         id: '2',
         nombre: 'Ultra white',
         color: 'blanco',
         precio: 1100,
-        stock: 50,
+        stock: 4,
         producto: 't-shirt',
         img: 'https://thejuice.com.ar/wp-content/uploads/2022/09/LUV9199-scaled.jpg',
-        descrpcion: ''
+        descripcion: 'Esta es una remera blanca...'
     },
     {
         id: '3',
         nombre: 'Zebra',
         color: 'negro',
         precio: 300,
-        stock: 100,
+        stock: 10,
         producto: 'socks',
         img: 'https://i.pinimg.com/564x/e6/23/d5/e623d5bb0c09eca6778c042ee238b948.jpg',
-        descrpcion: ''
+        descripcion: 'Estas son unas medias con rayas negra...'
     },
     {
         id: '4',
         nombre: 'Sun-sun',
         color: 'negro',
         precio: 700,
-        stock: 65,
+        stock: 3,
         producto: 'cap',
         img: 'https://i.pinimg.com/564x/22/93/4c/22934c0e4409ba92191e507b389a1406.jpg',
-        descrpcion: ''
+        descripcion: 'Esta es una gorra negra...'
     },
     {
         id: '5',
         nombre: 'Spice Grinder',
         color: ['blanco', 'rosa', 'gris'],
         precio: 500,
-        stock: 120,
+        stock: 2,
         producto: 'grinder',
         img: 'https://i.pinimg.com/564x/dd/18/d3/dd18d3db04ecd2836fa94af9c5629833.jpg',
-        descrpcion: ''
+        descripcion: 'instrumento de varios colores...'
     }
 ]
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,8 +12,7 @@ const Card = ({ producto, id }) => {
                 <div className="card-body">
                     <h5 className="card-title display-6">$ {producto.precio} </h5>
                     <p className="card-text">
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        {producto.descripcion}
                     </p>
                 </div>
             </div>
