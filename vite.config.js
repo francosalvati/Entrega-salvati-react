33abeace394e0cbdce5326dbb5e1abca470c84c1
franco salvati <francosalvati98@gmail.com>
2023-05-07 21:10:56 -0300

    baseUrll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://entrega-salvati-react.vercel.app/'
+  base: 'https://entrega-salvati-react-f30b53qhf-francosalvati.vercel.app/'
 })
